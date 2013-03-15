52d96175fe6f7f5a1f6e067548a93e7b60df78b9
Sander van der Burg <svanderburg@gmail.com>
2013-03-15 17:37:21 +0100

    Added replace script allowing you to rename the name of the App as well as the company, and some other attributes

--- deployment/helloworld/default.nix
+++ deployment/helloworld/default.nix
@@ -3,12 +3,12 @@
 xcodeenv.buildApp {
   name = "HelloWorld";
   src = ../../src/HelloWorld;
-  release = false;
-
-  /*certificateFile = /Users/sander/Downloads/c.p12;
+  
+  /*release = true;
+  certificateFile = ./certificate.p12;
   certificatePassword = "";
-  codeSignIdentity = "iPhone Distribution: My Company";  
-  provisioningProfile = /Users/sander/Downloads/AC911BE6-A06B-4C84-9460-BF6EF3287C9E.mobileprovision;
-  generateIPA = false;
-  generateXCArchive = true;*/
+  codeSignIdentity = "iPhone Distribution: My Company";
+  provisioningProfile = ./provision.mobileprovision;
+  generateIPA = true;
+  generateXCArchive = false;*/
 }
---
+++ rename.sh
@@ -0,0 +1,73 @@
+#!/bin/sh -e
+
+newName="$1"
+newId="$2"
+newDomain="$3"
+newCompanyName="$4"
+
+if [ $# -ne 4 ]
+then
+    echo "Usage:"
+    echo "$0 ByeWorld byeworld com.mycoolcompany \"My Cool Company\""
+    exit 1
+fi
+
+# Rename files that have the application name in them
+
+while true
+do
+    renameFile="$(find . -name HelloWorld\* | head -1)"
+
+    if [ "$renameFile" = "" ]
+    then
+       break;
+    fi
+
+    newFilename="$(echo "$renameFile" | sed "s%HelloWorld%$newName%")"
+
+    mv "$renameFile" "$newFilename"
+done
+
+# Replace file names that have the application identifier in the name
+
+while true
+do
+    renameFile="$(find . -name \*helloworld\* | head -1)"
+
+    if [ "$renameFile" = "" ]
+    then
+       break;
+    fi
+
+    newFilename="$(echo "$renameFile" | sed "s%helloworld%$newId%")"
+
+    mv "$renameFile" "$newFilename"
+done
+
+# Replace application name in contents
+
+find . -type f | while read i
+do
+    sed -i -e "s%HelloWorld%$newName%g" "$i"
+done
+
+# Replace application id in contents
+
+find . -type f | while read i
+do
+    sed -i -e "s%helloworld%$newId%g" "$i"
+done
+
+# Replace domain in contents
+
+find . -type f | while read i
+do
+    sed -i -e "s%MyCompany%$newDomain%g" "$i"
+done
+
+# Replace company in contents
+
+find . -type f | while read i
+do
+    sed -i -e "s%My Company%$newCompanyName%g" "$i"
+done
