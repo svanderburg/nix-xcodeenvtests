140463059f3c081cd9c68ead242b74abb22109ac
Sander van der Burg <svanderburg@gmail.com>
2013-05-29 15:02:30 +0200

    Override the appname with HelloWorld

--- deployment/simulate-helloworld/default.nix
+++ deployment/simulate-helloworld/default.nix
@@ -2,6 +2,7 @@
 
 xcodeenv.simulateApp {
   name = "simulate-${helloworld.name}-${stdenv.lib.replaceChars [" " "(" ")"] ["_" "" ""] device}";
+  appName = "HelloWorld";
   app = helloworld;
   inherit device;
 }
